20bafbc4fcb93fbfde13efa961a5a92c7cbc36d4
TheSUPERN0VA <79873066+TheSUPERN0VA@users.noreply.github.com>
2021-04-23 13:26:24 -0600

    Add files via upload

--- listAppV1.py
+++ listAppV1.py
@@ -9,6 +9,7 @@ Program Goals:
 """
 import random
 myList = []
+unique_list = []
 
 def mainProgram():
     
@@ -19,9 +20,14 @@ def mainProgram():
             choice = input("""1. Add to a list or
 2: Add a bunch of numbers
 3. Return a value at an index
-4. Random Search
-5. Print contents of list
-6. Quit program""")
+4. Sort List
+5. Random Search
+6. Recursive Binary Search
+7. Linear Search
+8. Recursive Binary Search
+9. Iterative Binary Search
+Print List
+Quit  """)
         #add a way to catch bad user responses
             if choice == "1":
                 addToList()
@@ -34,20 +40,39 @@ def mainProgram():
             elif choice == "5":
                 linearSearch()
             elif choice == "6":
-                print(myList)
-            else:
+                searchItem = input("What are you looking for?  ")
+                recursiveBinarySearch(unique_list, 0, len(unique_list)-1, int(searchItem))
+            elif choice == "7":
+                searchItem = input("What are you looking for?  ")
+                result = iterativeBinarySearch(unique_list, int(searchItem))
+                if result != -1:
+                    print("Your number is at index {}".format(result))
+                else:
+                    print("Your number isn't in that list!")
+            elif choice == "8":
+                sortList(myList)
+                print("Your number is not found in that list, bud!")
+            elif choice == "9":
+                printLists()
+            else:            
                 break
         except:
-                print("You made a whoopsie!")
+            print("You made a whoopsie!")
         
     
 def addToList():
+    """
+    This lets us add a single integer of our choice to the code
+    """
     print("Adding to a list! Great choice!")
     newItem = input("Type an integer here!   ")
     myList.append(int(newItem))
 
 def addABunch():
-    print("We're gonna add a bunch of integers here!")
+    """
+    This allows us to add several integers at once going up to a max value of our choice
+    """
+    print("We're gonna add a bunch of numbers to your list!")
     numToAdd = input("How many new integers would you like to add?  ")
     numRange = input("And how high would you like these numbers to go?  ")
     for x in range(0, int(numToAdd)):
@@ -55,20 +80,94 @@ def addABunch():
     print("Your list is now complete.")    
     
 def indexValues():
-    print("Ohhh! I heard you need a particular piece of data!")
-    indexPos = input("What index position are you curious about?   ")
+    """
+    This function will print the number at any index position on our list
+    """
+    print("At what index position do you want to search")
+    indexPos = input("Type n index position here:   ")
     print(myList[int(indexPos)])
 
+def sortList(myList):
+    """
+    With this we can sort the integers in the default list the program gives us and make a new one
+    """
+    print("A little bird told me you needed some data.")
+    for x in myList:
+        if x not in unique_list:
+            unique_list.append(x)
+    unique_list.sort()
+    showMe = input("Wanna see your new list? Y/N  ")
+    if showMe.lower() == "y":
+        print(unique_list)
+
 def randomSearch():
+    """
+    This function will give us a random number from the list
+    """
     print("RaNdOm SeArCh?!?")
     print(myList[random.randint(0, len(myList)-1)])
+    if len(unique_list) > 0:
+           print(unique_list[random.randint(0, len(unique_list)-1)])
     
 def linearSearch():
+    """
+    This will ma e the program go through each index postition one at a time and give us every index position where a number we want is at
+    """
     print("We're gonna check out each item one at a time in your list. This sucks!")
-    searchItem = input("What you lookin for pardner?  ")
+    searchValue = input("What you looking for?  ")
     for x in range(len(myList)):
-        if myList[x] == int(searchItem):
+        if myList[x] == int(searchValue):
             print("Your item is at index position {}".format(x))
+
+def recursiveBinarySearch(unique_list, low, high, x):
+    """
+    This will print the number we want over and over every time it's on the list
+    """
+    if high >= low:
+        mid = (high + low) // 2
+
+        if unique_list[mid] == x:
+            print("You ding dang found it at index position {}".format(mid))
+            return mid
+        elif unique_list[mid] >= x:
+            return recuriveBinarySearch(unique_list, low, mid -1, x)
+
+        elif unique_list[mid] > x:
+            return recursiveBinarySearch(unique_list, low, mid - 1, x)
+        else:
+            return recursiveBinarySearch(unique_list, mide + 1, high, x)
+    else:
+        print("Your number isn't here!")
+
+def iterativeBinarySearch(unique_list, x):
+    """
+    This function will look for the number you want, showing every number along the way utnil it find what you want
+    """
+    low = 0
+    high = len(unique_list)-1
+    mid = 0
+
+    while low <= high:
+        mid = (high + low) // 2
+
+        if unique_list[mid] < x:
+            low = mid - 1
+        else:
+                return mid
+    return -1
+
+def printLists():
+    """
+    Self explanatory. It prints your list out entirely
+    """
+    if len(unique_list) == 0:
+        print(myList)
+    else:
+            whichOne = input("Which list? Sorted or unsorted?  ")
+            if whichOne.lower() == "sorted":
+                print(unique_list)
+            else:
+                    print(myList)
     
 if __name__ == "__main__":
     mainProgram()
---
+++ readMeFINAL.txt
@@ -0,0 +1,9 @@
+The software is overall rpretty easy to use. Start the program and you'll be met with several options with what you want to do.
+You can add one individual integer of your choice, add a bunch or random numbers up to a maximum value, sort the integers,
+Look for a specific number and see if it's there, ask for a random number for the list, or print the whole list out entirely.
+
+The recursive binary search allows you to ask for a specific number and the program will print it over and over for every position.
+The iterative binary search will run on a while loop and look for the number and stop once it prints the number, showing every number until the program finds it.
+
+A change I would've liked to make was to combine some functions if we could. This would make the code easier to go through and less tedious to debug and add to.
+It definitely would've helped me work faster since I don't need to go through so much small text at once.
